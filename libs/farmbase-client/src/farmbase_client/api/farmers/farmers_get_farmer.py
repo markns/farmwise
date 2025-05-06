d6bda394f634a312638eb69f309249267538e4fd
Mark Nuttall-Smith <nuttall@apolloagriculture.com>
2025-05-06 17:50:04 +0300

    regenerated farmbase client

---
+++ libs/farmbase-client/src/farmbase_client/api/farmers/farmers_get_farmer.py
@@ -0,0 +1,203 @@
+from http import HTTPStatus
+from typing import Any, Optional, Union, cast
+
+import httpx
+
+
+from ...client import AuthenticatedClient, Client
+from ...types import Response, UNSET
+from ... import errors
+
+from ...models import ErrorResponse
+from ...models import FarmerRead
+from ...models import HTTPValidationError
+from typing import cast
+
+
+def _get_kwargs(
+    organization: str,
+    farmer_id: int,
+) -> dict[str, Any]:
+    _kwargs: dict[str, Any] = {
+        "method": "get",
+        "url": "/{organization}/farmers/{farmer_id}".format(
+            organization=organization,
+            farmer_id=farmer_id,
+        ),
+    }
+
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    if response.status_code == 200:
+        response_200 = FarmerRead.model_validate(response.json())
+
+        return response_200
+    if response.status_code == 400:
+        response_400 = ErrorResponse.model_validate(response.json())
+
+        return response_400
+    if response.status_code == 401:
+        response_401 = ErrorResponse.model_validate(response.json())
+
+        return response_401
+    if response.status_code == 403:
+        response_403 = ErrorResponse.model_validate(response.json())
+
+        return response_403
+    if response.status_code == 404:
+        response_404 = ErrorResponse.model_validate(response.json())
+
+        return response_404
+    if response.status_code == 500:
+        response_500 = ErrorResponse.model_validate(response.json())
+
+        return response_500
+    if response.status_code == 422:
+        response_422 = HTTPValidationError.model_validate(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    organization: str,
+    farmer_id: int,
+    *,
+    client: Union[AuthenticatedClient, Client],
+) -> Response[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    """Get a farmer.
+
+     Get a farmer.
+
+    Args:
+        organization (str):
+        farmer_id (int):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorResponse, FarmerRead, HTTPValidationError]]
+    """
+
+    kwargs = _get_kwargs(
+        organization=organization,
+        farmer_id=farmer_id,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    organization: str,
+    farmer_id: int,
+    *,
+    client: Union[AuthenticatedClient, Client],
+) -> Optional[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    """Get a farmer.
+
+     Get a farmer.
+
+    Args:
+        organization (str):
+        farmer_id (int):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorResponse, FarmerRead, HTTPValidationError]
+    """
+
+    return sync_detailed(
+        organization=organization,
+        farmer_id=farmer_id,
+        client=client,
+    ).parsed
+
+
+async def asyncio_detailed(
+    organization: str,
+    farmer_id: int,
+    *,
+    client: Union[AuthenticatedClient, Client],
+) -> Response[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    """Get a farmer.
+
+     Get a farmer.
+
+    Args:
+        organization (str):
+        farmer_id (int):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorResponse, FarmerRead, HTTPValidationError]]
+    """
+
+    kwargs = _get_kwargs(
+        organization=organization,
+        farmer_id=farmer_id,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    organization: str,
+    farmer_id: int,
+    *,
+    client: Union[AuthenticatedClient, Client],
+) -> Optional[Union[ErrorResponse, FarmerRead, HTTPValidationError]]:
+    """Get a farmer.
+
+     Get a farmer.
+
+    Args:
+        organization (str):
+        farmer_id (int):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorResponse, FarmerRead, HTTPValidationError]
+    """
+
+    return (
+        await asyncio_detailed(
+            organization=organization,
+            farmer_id=farmer_id,
+            client=client,
+        )
+    ).parsed
---
+++ libs/farmbase-client/src/farmbase_client/api/projects/projects_get_projects.py
@@ -0,0 +1,295 @@
+from http import HTTPStatus
+from typing import Any, Optional, Union, cast
+
+import httpx
+
+
+from ...client import AuthenticatedClient, Client
+from ...types import Response, UNSET
+from ... import errors
+
+from ...models import ErrorResponse
+from ...models import HTTPValidationError
+from ...models import ProjectPagination
+from ...types import UNSET, Unset
+from typing import cast
+from typing import Union
+
+
+def _get_kwargs(
+    organization: str,
+    *,
+    page: Union[Unset, int] = 1,
+    items_per_page: Union[Unset, int] = 5,
+    q: Union[Unset, str] = UNSET,
+    filter_: Union[Unset, str] = UNSET,
+    sort_by: Union[Unset, list[str]] = UNSET,
+    descending: Union[Unset, list[bool]] = UNSET,
+) -> dict[str, Any]:
+    params: dict[str, Any] = {}
+
+    params["page"] = page
+
+    params["items_per_page"] = items_per_page
+
+    params["q"] = q
+
+    params["filter"] = filter_
+
+    json_sort_by: Union[Unset, list[str]] = UNSET
+    if not isinstance(sort_by, Unset):
+        json_sort_by = sort_by
+
+    params["sortBy[]"] = json_sort_by
+
+    json_descending: Union[Unset, list[bool]] = UNSET
+    if not isinstance(descending, Unset):
+        json_descending = descending
+
+    params["descending[]"] = json_descending
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    _kwargs: dict[str, Any] = {
+        "method": "get",
+        "url": "/{organization}/projects".format(
+            organization=organization,
+        ),
+        "params": params,
+    }
+
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    if response.status_code == 200:
+        response_200 = ProjectPagination.model_validate(response.json())
+
+        return response_200
+    if response.status_code == 400:
+        response_400 = ErrorResponse.model_validate(response.json())
+
+        return response_400
+    if response.status_code == 401:
+        response_401 = ErrorResponse.model_validate(response.json())
+
+        return response_401
+    if response.status_code == 403:
+        response_403 = ErrorResponse.model_validate(response.json())
+
+        return response_403
+    if response.status_code == 404:
+        response_404 = ErrorResponse.model_validate(response.json())
+
+        return response_404
+    if response.status_code == 500:
+        response_500 = ErrorResponse.model_validate(response.json())
+
+        return response_500
+    if response.status_code == 422:
+        response_422 = HTTPValidationError.model_validate(response.json())
+
+        return response_422
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    organization: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    page: Union[Unset, int] = 1,
+    items_per_page: Union[Unset, int] = 5,
+    q: Union[Unset, str] = UNSET,
+    filter_: Union[Unset, str] = UNSET,
+    sort_by: Union[Unset, list[str]] = UNSET,
+    descending: Union[Unset, list[bool]] = UNSET,
+) -> Response[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    """Get Projects
+
+     Get all projects.
+
+    Args:
+        organization (str):
+        page (Union[Unset, int]):  Default: 1.
+        items_per_page (Union[Unset, int]):  Default: 5.
+        q (Union[Unset, str]):
+        filter_ (Union[Unset, str]):
+        sort_by (Union[Unset, list[str]]):
+        descending (Union[Unset, list[bool]]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]
+    """
+
+    kwargs = _get_kwargs(
+        organization=organization,
+        page=page,
+        items_per_page=items_per_page,
+        q=q,
+        filter_=filter_,
+        sort_by=sort_by,
+        descending=descending,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    organization: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    page: Union[Unset, int] = 1,
+    items_per_page: Union[Unset, int] = 5,
+    q: Union[Unset, str] = UNSET,
+    filter_: Union[Unset, str] = UNSET,
+    sort_by: Union[Unset, list[str]] = UNSET,
+    descending: Union[Unset, list[bool]] = UNSET,
+) -> Optional[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    """Get Projects
+
+     Get all projects.
+
+    Args:
+        organization (str):
+        page (Union[Unset, int]):  Default: 1.
+        items_per_page (Union[Unset, int]):  Default: 5.
+        q (Union[Unset, str]):
+        filter_ (Union[Unset, str]):
+        sort_by (Union[Unset, list[str]]):
+        descending (Union[Unset, list[bool]]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorResponse, HTTPValidationError, ProjectPagination]
+    """
+
+    return sync_detailed(
+        organization=organization,
+        client=client,
+        page=page,
+        items_per_page=items_per_page,
+        q=q,
+        filter_=filter_,
+        sort_by=sort_by,
+        descending=descending,
+    ).parsed
+
+
+async def asyncio_detailed(
+    organization: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    page: Union[Unset, int] = 1,
+    items_per_page: Union[Unset, int] = 5,
+    q: Union[Unset, str] = UNSET,
+    filter_: Union[Unset, str] = UNSET,
+    sort_by: Union[Unset, list[str]] = UNSET,
+    descending: Union[Unset, list[bool]] = UNSET,
+) -> Response[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    """Get Projects
+
+     Get all projects.
+
+    Args:
+        organization (str):
+        page (Union[Unset, int]):  Default: 1.
+        items_per_page (Union[Unset, int]):  Default: 5.
+        q (Union[Unset, str]):
+        filter_ (Union[Unset, str]):
+        sort_by (Union[Unset, list[str]]):
+        descending (Union[Unset, list[bool]]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]
+    """
+
+    kwargs = _get_kwargs(
+        organization=organization,
+        page=page,
+        items_per_page=items_per_page,
+        q=q,
+        filter_=filter_,
+        sort_by=sort_by,
+        descending=descending,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    organization: str,
+    *,
+    client: Union[AuthenticatedClient, Client],
+    page: Union[Unset, int] = 1,
+    items_per_page: Union[Unset, int] = 5,
+    q: Union[Unset, str] = UNSET,
+    filter_: Union[Unset, str] = UNSET,
+    sort_by: Union[Unset, list[str]] = UNSET,
+    descending: Union[Unset, list[bool]] = UNSET,
+) -> Optional[Union[ErrorResponse, HTTPValidationError, ProjectPagination]]:
+    """Get Projects
+
+     Get all projects.
+
+    Args:
+        organization (str):
+        page (Union[Unset, int]):  Default: 1.
+        items_per_page (Union[Unset, int]):  Default: 5.
+        q (Union[Unset, str]):
+        filter_ (Union[Unset, str]):
+        sort_by (Union[Unset, list[str]]):
+        descending (Union[Unset, list[bool]]):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Union[ErrorResponse, HTTPValidationError, ProjectPagination]
+    """
+
+    return (
+        await asyncio_detailed(
+            organization=organization,
+            client=client,
+            page=page,
+            items_per_page=items_per_page,
+            q=q,
+            filter_=filter_,
+            sort_by=sort_by,
+            descending=descending,
+        )
+    ).parsed
