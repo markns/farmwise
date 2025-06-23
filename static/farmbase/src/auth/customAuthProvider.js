3232b4396ce00d8bdd16c59b0fa8215ceb92f8c8
Mark Nuttall-Smith <nuttall@apolloagriculture.com>
2025-06-23 11:24:00 +0300

    move react app into apps/frontend.

--- static/farmbase/src/auth/customAuthProvider.js
+++
@@ -1,7 +0,0 @@
-function login(to, from, next) {
-  next()
-}
-
-export default {
-  login,
-}
--- static/farmbase/src/components/layouts/index.js
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from "@/components/layouts/DefaultLayout.vue"
-import DashboardLayout from "@/components/layouts/DashboardLayout.vue"
-import BasicLayout from "@/components/layouts/BasicLayout.vue"
-
-export { BasicLayout, DefaultLayout, DashboardLayout }
