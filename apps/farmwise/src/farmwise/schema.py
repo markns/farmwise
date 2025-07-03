b4d1f9809a4392150678eace40d079a0d1c70950
Mark Nuttall-Smith <nuttall@apolloagriculture.com>
2025-07-03 17:44:22 +0300

    Removed unused code

--- apps/farmwise/src/farmwise/schema.py
+++ apps/farmwise/src/farmwise/schema.py
@@ -1,10 +1,8 @@
 import pathlib
 from enum import Enum
-from typing import Any, Literal, NotRequired
 
 from pydantic import BaseModel, Field, ConfigDict
 from pywa.types import Button, SectionList
-from typing_extensions import TypedDict
 
 
 class AgentInfo(BaseModel):
@@ -41,70 +39,11 @@ class UserInput(BaseModel):
         description="Voice path to send to the agent.",
         default=None,
     )
-    # user_id: str = Field(
-    #     description="User ID to persist and continue a multi-turn conversation.",
-    #     examples=["+254748256530", "847c6285-8fc9-4560-a83f-4e6285809254"],
-    # )
-    # timestamp: datetime = Field(
-    #     description="Timestamp of the user input.",
-    #     default_factory=lambda: datetime.now(UTC),
-    #     examples=[datetime.now(UTC).isoformat()],
-    # )
-    # user_name: str | None = Field(
-    #     description="User ID to persist and continue a multi-turn conversation.",
-    #     default=None,
-    #     examples=["Mark Nuttall-Smith"],
-    # )
-    # agent_config: dict[str, Any] = Field(
-    #     description="Additional configuration to pass through to the agent",
-    #     default={},
-    #     examples=[{"spicy_level": 0.8}],
-    # )
 
-
-class StreamInput(UserInput):
-    """User input for streaming the agent's response."""
-
-    stream_tokens: bool = Field(
-        description="Whether to stream LLM tokens to the client.",
-        default=True,
-    )
-
-
-class ToolCall(TypedDict):
-    """Represents a request to call a tool."""
-
-    name: str
-    """The name of the tool to be called."""
-    args: dict[str, Any]
-    """The arguments to the tool call."""
-    id: str | None
-    """An identifier associated with the tool call."""
-    type: NotRequired[Literal["tool_call"]]
 
 
 class Action(Enum):
     request_location = "request_location"
-
-#
-# class Button(BaseModel):
-#     title: str
-#     callback_data: str
-#
-#
-# class SectionRow(BaseModel):
-#     title: Annotated[str, StringConstraints(max_length=24)]
-#     callback_data: str
-#
-#
-# class Section(BaseModel):
-#     title: Annotated[str, StringConstraints(max_length=24)]
-#     rows: Annotated[list[SectionRow], Len(min_length=1, max_length=10)]
-#
-#
-# class SectionList(BaseModel):
-#     button_title: str
-#     sections: list[Section]
 
 
 class Contact(BaseModel):
@@ -159,47 +98,3 @@ class ResponseEvent(BaseModel):
     response: TextResponse | AudioResponse
     has_more: bool = True
 
-
-class Feedback(BaseModel):
-    """Feedback for a run, to record to LangSmith."""
-
-    run_id: str = Field(
-        description="Run ID to record feedback for.",
-        examples=["847c6285-8fc9-4560-a83f-4e6285809254"],
-    )
-    key: str = Field(
-        description="Feedback key.",
-        examples=["human-feedback-stars"],
-    )
-    score: float = Field(
-        description="Feedback score.",
-        examples=[0.8],
-    )
-    kwargs: dict[str, Any] = Field(
-        description="Additional feedback kwargs, passed to LangSmith.",
-        default={},
-        examples=[{"comment": "In-line human feedback"}],
-    )
-
-
-class FeedbackResponse(BaseModel):
-    status: Literal["success"] = "success"
-
-
-class ChatHistoryInput(BaseModel):
-    """Input for retrieving chat history."""
-
-    user_id: str | None = Field(
-        description="User ID to persist and continue a multi-turn conversation.",
-        default=None,
-        examples=["+254748256530", "847c6285-8fc9-4560-a83f-4e6285809254"],
-    )
-    thread_id: str = Field(
-        description="Thread ID to persist and continue a multi-turn conversation.",
-        examples=["847c6285-8fc9-4560-a83f-4e6285809254"],
-    )
-
-
-class ChatHistory(BaseModel):
-    ...
-    # messages: list[ChatMessage]
