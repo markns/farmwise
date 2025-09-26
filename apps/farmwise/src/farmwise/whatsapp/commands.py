be9f8ed284a93ded553f9f8924b1941d3bedb87b
Mark Nuttall-Smith <nuttall@apolloagriculture.com>
2025-09-26 06:26:12 +0300

    Added whatsapp flow for managing farmbetter profile

--- apps/farmwise/src/farmwise/whatsapp/commands.py
+++ apps/farmwise/src/farmwise/whatsapp/commands.py
@@ -1,8 +1,7 @@
 from pywa_async.types import Command
 
-# TODO: load these commands from the FarmWise service
 commands = [
     Command(name="menu", description="Show activities"),
-    # Command(name="profile", description="Show profile"),
-    # Command(name="login", description="Login"),
+    Command(name="profile", description="Edit your farmbetter profile"),
+    Command(name="location", description="Set the location of your farm"),
 ]
