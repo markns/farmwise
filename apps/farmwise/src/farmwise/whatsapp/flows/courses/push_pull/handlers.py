d3db980956acc92fe6e0bf097e3a94fdf237256d
Mark Nuttall-Smith <nuttall@apolloagriculture.com>
2025-09-30 15:13:36 +0300

    remove unused module

--- apps/farmwise/src/farmwise/whatsapp/flows/courses/push_pull/handlers.py
+++
@@ -1,6 +0,0 @@
-# TODO: received a FlowCompletion to the edit_profile flow when completing the course flow
-
-#   File "/Users/markns/workspace/farmwise/apps/farmwise/src/farmwise/whatsapp/flows/edit_profile/handlers.py", line 89, in on_edit_profile_completion
-#     poultry = flow.response.pop("poultry")
-#               │    └ <member 'response' of 'FlowCompletion' objects>
-#               └ FlowCompletion(id='wamid.HBgLMzE2NTc3NzU3ODEVAgASGCBBQzE1Q0JBN0YwREY2QkQyOUU0MzI0MkEzOUVFRDIyOQA=', timestamp=datetime.dateti...
